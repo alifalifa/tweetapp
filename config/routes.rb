79167879e6344e7bbb0d67482f18e41c18ad56fe
febiyani paneo <febiyani_s1arsitektur@mahasiswa.ung.ac.id>
2022-08-15 23:08:25 +0800

    fixing create post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  
   get "/" => "home#top"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about" => "home#about"
   # Defines the root path route ("/")
